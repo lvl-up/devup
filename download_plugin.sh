99ddb969eb2ee42c61f1ecddfeba526b6e4bc96f
leon <leon_a_d@yahoo.com>
2013-11-08 02:51:29 +0000

    downloads any plugin now

--- download_plugin.sh
+++ download_plugin.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-function find_plugin_page(){
-  local slim_url=$(curl -s 'http://plugins.jetbrains.com/search/index?pr=idea&search=slim' | grep -E "<a.*?><span.*?>$1")
+jetbrains_base_url='http://plugins.jetbrains.com'
 
-  if [[ "${slim_url}" =~ href=\"(.*)\" ]]
-  then
-     echo "http://plugins.jetbrains.com${BASH_REMATCH[1]}"
-  else
-    echo "plugin not found"
-    exit 1
-  fi
-
-
-}
 function find_match(){
     echo $(echo "$1" | perl -p -e "s/$2/\1/")
 }
 
-function find_plugin_download_url(){
-  local result=$(curl -s $1 | tr -d '\n' | perl -p -e 's/&amp;/&/g' )
-  local row=$(find_match "${result}" '.*(6.0.0.20131106.*?\/tr).*')
-  local url=$(find_match "${row}" '.*?downld".*?href="(.*?)".*')
-  echo "http://plugins.jetbrains.com${url}"
-}
-plugin_page_url=$(find_plugin_page "${1}")
-echo "${plugin_page_url}"
+function find_plugin_page(){
+  local search_results=$(curl -s "${jetbrains_base_url}/search/index?pr=idea&search=$1" | tr -d '\n')
 
-plugin_download_url=$(find_plugin_download_url "${plugin_page_url}")
+  local plugin_uri=$(find_match "${search_results}" ".*?dl class=\"category\".*?href=\"(.*?)\"><span.*?>$1<\/span.*" )
+
+  echo "${jetbrains_base_url}${plugin_uri}"
+}
+
+
+function find_plugin_download_url(){
+  local plugin_page_url=$1
+  local version=$2
+
+  local plugin_page=$(curl -s ${plugin_page_url} | tr -d '\n' | perl -p -e 's/&amp;/&/g' )
+  local row_with_version_in_it=$(find_match "${plugin_page}" ".*(${version}.*?\/tr).*")
+  local download_uri=$(find_match "${row_with_version_in_it}" '.*?downld".*?href="(.*?)".*')
+  echo "${jetbrains_base_url}${download_uri}"
+}
+
+plugin_page_url=$(find_plugin_page "${1}")
+plugin_download_url=$(find_plugin_download_url "${plugin_page_url}" $2)
 echo "$plugin_download_url"
 wget --content-disposition "$plugin_download_url"
